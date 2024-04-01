5ddb58e1f3aeb02a44cf97cb046a7b8974b45531
vinicius <vinny.db@hotmail.com>
2024-04-01 20:57:35 -0300

    programa atualizado - inclui outras formas de pagto  e informativos

--- src/index.js
+++ src/index.js
@@ -1,25 +1,43 @@
+const prompt = require('prompt-sync')({ sigint: true });
 
-    const prompt = require('prompt-sync')({sigint:true})
+let valordaMesa = [];
 
-    let valordaMesa = [];
-
-    while (true) {
+while (true) {
     console.log("\nEscolha uma opção:");
     console.log("1. Calcular a conta:");
     console.log("2. Sair");
-  
     let opcao = prompt("Opção: ");
-
+    
+    console.log();
     if (opcao === "1") {
         let numMesa = parseInt(prompt("Informe o numero da mesa: "));
         let numPessoas = parseInt(prompt("Informe o numero de pessoas na mesa: "));
         let valorTotal = parseFloat(prompt("Informe o valor total da mesa: "));
-        
-        console.log("Informe a forma de pagamento:");
+
+        console.log("\nInforme a forma de pagamento:");
         console.log("1. Pix");
         console.log("2. Dinheiro");
-        console.log("3. Cartão");
+        console.log("3. Cartão de Crédito");
+        console.log("4. Cartão de Debito");
         let metodoPagamento = prompt("Opção: ");
+
+        
+        let totalComDesconto;
+        if (metodoPagamento === "1" || metodoPagamento === "2") {
+            totalComDesconto = valorTotal * 0.9;
+
+            
+            console.log("\nValor com desconto de 10%");
+            console.log();
+        } else {
+            totalComDesconto = valorTotal;
+
+            
+            console.log("\nValor sem desconto");
+            console.log();
+        }
+
+        let valorPorPessoa = totalComDesconto / numPessoas;
 
         switch (metodoPagamento) {
             case "1":
@@ -29,41 +47,35 @@
                 metodoPagamento = "Dinheiro";
                 break;
             case "3":
-                metodoPagamento = "Cartão";
+                metodoPagamento = "Cartão de Crédito";
+                break;
+            case "4":
+                metodoPagamento = "Cartão de Debito";
                 break;
             default:
                 metodoPagamento = "Desconhecido, tente novamente.";
         }
 
-        let  totalComDesconto = valorTotal * 0.9;
-
-        let  valorPorPessoa = totalComDesconto / numPessoas;
-
-
         let conta = {
-         mesa: numMesa,
-         pessoas: numPessoas,
-         total: valorTotal,
-         pagamento: metodoPagamento,
-         totalComDesconto: totalComDesconto,
-         valorPorPessoa: valorPorPessoa
-
-
+            mesa: numMesa,
+            pessoas: numPessoas,
+            total: valorTotal,
+            pagamento: metodoPagamento,
+            totalComDesconto: totalComDesconto,
+            valorPorPessoa: valorPorPessoa
         };
+
         valordaMesa.push(conta);
 
-        console.log("Valor com desconto de 10%");
-        
         for (let i = 0; i < valordaMesa.length; i++) {
             console.log(`Mesa: ${valordaMesa[i].mesa}, Pessoas: ${valordaMesa[i].pessoas}, Total: ${valordaMesa[i].total.toFixed(2)}, Pagamento: ${valordaMesa[i].pagamento}, Total com Desconto: ${valordaMesa[i].totalComDesconto.toFixed(2)}, Valor por Pessoa: ${valordaMesa[i].valorPorPessoa.toFixed(2)}`);
         }
+        console.log();
+        console.log("O desconto só é aplicado quando a mesa fecha o pagamento à vista ou pix, caso contrário o desconto não é aplicado. Conta calculada com sucesso! ");
         
-        console.log("Conta calculada com sucesso!");
-    }
-// Aqui você pode fazer qualquer coisa que queira após sair do loop, por exemplo, imprimir todas as contas registradas.
-        else if (opcao === "2") {
-        // Sair do loop e encerrar o programa
+        
+    } else if (opcao === "2") {
+        
         break;
     }
 }
-
